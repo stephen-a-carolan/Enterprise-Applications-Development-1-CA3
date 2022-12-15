660e81752e68eadb040be6e40cbef529c4132544
Stephen Carolan <X00145825@mytudublin.ie>
2022-12-15 07:55:49 +0000

    new branch

---
+++ Service/IDateService.cs
@@ -0,0 +1,6 @@
+﻿namespace Enterprise_Applications_Development_1_CA3.Service
+{
+    public interface IDateService
+    {
+    }
+}
--- wwwroot/Models/ItemData.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Enterprise_Applications_Development_1_CA3.wwwroot.Models
-{
-    public class ItemData
-    {
-        
-    }
-}
